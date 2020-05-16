d05af39e142c532bc9c155530b63163aedd5396c
Andres Alba <bryanalba92@gmail.com>
2020-05-15 21:33:51 -0500

    Form register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-register',
@@ -6,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public user: User;
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) {
+      this.user = new User( '', '', '', '', '', '', 'ROLE_USER', '');
+    }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log(this.user);
   }
 
 }
